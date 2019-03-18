bd302041c2495efc9d8dab533c27207d416bfe4b
note <student11@Student11.local>
2019-03-18 14:28:14 +0700

    Start Data Type

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+//String Type ตัวแปรที่เป็นอักษร
+
+var myName = "Doramon" //นี่คือการประกาศ ที่อาศันชนิดของ value บอก Datatype
+var mySurname:String = "AAAAAA" //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
+
+
+
+
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//Explicti Variable
+//การประกายตัวแปร นะจ๊ะ
+
+var myName = "Note"
+myName = "Doramon"
+
+//Explicti Constant
+let myFriend = "Nobita"
+//myFriend = "Sunako"Error เพราะ myFriend เป็นคำคงที่
+
+
