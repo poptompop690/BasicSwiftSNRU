268d88983769976e892f910efabad63a535e162a
note <student11@Student11.local>
2019-03-19 15:43:42 +0700

    Class and Function

--- DataTypeOfVariable.playground/Contents.swift
+++ DataTypeOfVariable.playground/Contents.swift
@@ -6,6 +6,38 @@ var myName = "Doramon" //นี่คือการประกาศ ที่
 var mySurname:String = "AAAAAA" //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
 
 
+//การแสดงผลบน Console
+print("This is Console")
+print("myName =\(myName)")
+
+//lnteger Type หรือ จำนวนที่ไม่มีเศษ
+var number1 = 100
+var number2:Int = 200
+
+//Double Type หรือ จำนวนที่มีเศษ
+var number3 = 3.1416
+var number4:Double = 10.234
+
+//Boolean Type คือ จำนวนที่มีแค่ค่า true,false
+var status = true
+var status2:Bool = false
+
+//Array Type
+var myFriends = ["Doramon","Nopita","Sunako","Chisuka"]
+print("myFriends ที่มี index = 1 มีค่าเท่ากับ =>\(myFriends[1])")
+
+//การเพิ่มสมาชิดให้กับ Array
+myFriends.append("MasterUNG")
+print(myFriends)
+
+//การลบสมาชิดของ Array
+myFriends.remove(at: 1)
+print(myFriends)
+
+
+
+
+
 
 
 
